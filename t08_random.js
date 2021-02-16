56fe3719e5fa1be6aa2cee76153207d172cbfc78
Loading Name <19410tc@hvhs.school.nz>
2021-02-16 08:03:19 +0000

    finished task 8 starting on task 9

--- t08_random.js
+++ t08_random.js
@@ -20,13 +20,27 @@ var canvheight = 400;
 var r = 225;
 var g = 225;
 var b = 225;
+var ballC ={
+	r:255,
+	g:255,
+	b:255
+};
 
 
 function setup() {
   createCanvas(canvwidth, canvheight);
+	ball.x = random (0, 90);
+	ball.y = random (100, 200);
+	bball.x = random (10, 100);
+	bball.y = random (200, 300)
 }
 function draw() {
   background(r, g, b);
+	ballC.r = random (50, 250);
+	ballC.g = random (50, 250);
+	ballC.b = random (50, 250);
+	fill(ballC.r, ballC.g, ballC.b);
+	noStroke();
 	ellipse(ball.x, ball.y, ball.dia);
 	if (ball.x >= (canvwidth - ball.dia/2)){
 		speedX = -3;

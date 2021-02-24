91d24db68711e12f171cb9e6e635c0ffa4ce2f45
Loading Name <19410tc@hvhs.school.nz>
2021-02-24 22:05:53 +0000

    ming changed it, lmao

--- t10_array_of_objects.js
+++ t10_array_of_objects.js
@@ -1,6 +1,7 @@
 /********************************************************/
-//t10_array_of_objects.js
+//t09_functions.js
 /********************************************************/
+var lmao = [];
 var ball = {
 	x: 0,
  	y: 100,
@@ -12,6 +13,7 @@ var ball = {
 	b:255,
 
 	aball:function(){
+	ball.dia = sSlider.value();
 	if (ball.x >= (bgs.canvwidth - ball.dia/2)){
 		ball.speedX =ball.speedX * -1;
 	}
@@ -29,37 +31,7 @@ var ball = {
 	ball.y = ball.y + ball.speedY;
  }
 };
-var bball = {
-	x: 10,
- 	y: 200,
-	dia: 50,
-	speedB: 3,
-	speedD: 3,
-	
-	abball: function(){
-	if (bball.x >= (bgs.canvwidth - bball.dia/2)){
-		bball.speedB = -3
-	}
-	ellipse(bball.x, bball.y, bball.dia)
-	if (bball.x <= bgs.canvwidth - bgs.canvwidth + bball.dia/2){
-		bball.speedB = 3;
-	}
-	if (bball.y >= bgs.canvheight - bball.dia/2){
-		bball.speedD = -3;
-	}
-	if (bball.y <= bgs.canvheight - bgs.canvheight + bball.dia/2){
-		bball.speedD = 3;
-	}
-	bball.x = bball.x + bball.speedB;
-	bball.y = bball.y + bball.speedD;
- }
 
-};
-var ballC ={
-	r:255,
-	g:255,
-	b:255
-};
 var bgs ={
 	canvwidth: 400,
 	canvheight: 400,
@@ -76,20 +48,19 @@ function setup() {
   createCanvas(bgs.canvwidth, bgs.canvheight);
 	ball.x = random (10, 390);
 	ball.y = random (10, 390);
-	bball.x = random (10, 390);
-	bball.y = random (10, 390);
-
+	lmao[0] = ball;
+	lmao[0].ini;
 	sSlider = createSlider(0, 255, 100);
   sSlider.position(20, 20);
+	
 }
 function draw() {
-  background(bgs.r, bgs.g, bgs.b, /*t*/);
-	ball.r = random (50, 250);
-	ball.g = random (50, 250);
-	ball.b = random (50, 250);
-	fill(ball.r, ball.g, ball.b);
+  background(bgs.r, bgs.g, bgs.b, /*bgs.t*/);
+	lmao[0].r = random (50, 250);
+	lmao[0].g = random (50, 250);
+	lmao[0].b = random (50, 250);
+	fill(lmao[0].r, lmao[0].g, lmao[0].b);
 	noStroke();
-	ball.aball();
-	bball.abball();
+	lmao[0].aball();
 
 }

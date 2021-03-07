d3e5697d6730ffd8e652fdf44ae1a904b078a0db
Loading Name <19410tc@hvhs.school.nz>
2021-03-07 04:00:54 +0000

    just trying out new timer code

--- t11_timer.js
+++ t11_timer.js
@@ -8,3 +8,11 @@ function setup() {
 function draw() {
   background(220);
 }
+
+
+ var sec = 0;
+	function pad ( value ) { return value > 9 ? value : "0" + value; }
+    setInterval( function(){
+        $("#seconds").html(pad(++sec%60));
+        $("#minutes").html(pad(parseInt(sec/60,10)));
+	}, 1000);
--- t12_mouse_events.js
+++ t12_mouse_events.js
@@ -1,11 +1,12 @@
 /********************************************************/
 //t12_mouse_events.js
 /********************************************************/
-var button = createButton();
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+	createButton();
 }
